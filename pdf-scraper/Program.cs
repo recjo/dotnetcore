d8ade78f30dd760bf09d8f70e4904ad97e28a48b
recjo <40804767+recjo@users.noreply.github.com>
2019-02-24 13:02:09 -0800

    inital upload

---
+++ pdf-scraper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace citycrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cls = new Class1();
+            cls.Go();
+        }
+    }
+}
